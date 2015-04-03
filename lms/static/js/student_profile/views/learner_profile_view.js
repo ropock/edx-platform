fd85b5175e6d142bb605289b137e0bb1b7e43db9
Usman Khalid <2200617@gmail.com>
2015-04-03 15:09:07 +0500

    Initial code.

--- lms/static/js/student_profile/views/learner_profile_view.js
+++ lms/static/js/student_profile/views/learner_profile_view.js
@@ -22,8 +22,6 @@
 
             render: function () {
                 this.$el.html(this.template({
-                    username: this.options.accountSettingsModel.get('username'),
-                    profilePhoto: 'http://www.teachthought.com/wp-content/uploads/2012/07/edX-120x120.jpg',
                     ownProfile: this.options.own_profile,
                     showFullProfile: this.showFullProfile()
                 }));
@@ -44,6 +42,7 @@
                 }
 
                 this.$('.profile-section-one-fields').append(this.options.usernameFieldView.render().el);
+                this.$('.profile-image-field').append(this.options.profileImageFieldView.render().el);
 
                 if (this.showFullProfile()) {
                     _.each(this.options.sectionOneFieldViews, function (fieldView, index) {
