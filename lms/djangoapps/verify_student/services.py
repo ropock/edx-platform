c77730c3bdca68d1f2116757f6bf9588e8d277ec
aamir-khan <aamir@edx.org>
2015-04-06 17:20:58 +0500

    ECOM-1289: added the documentation of service

--- lms/djangoapps/verify_student/services.py
+++ lms/djangoapps/verify_student/services.py
@@ -5,8 +5,21 @@ from verify_student.models import VerificationCheckpoint, VerificationStatus
 
 
 class ReverificationService(object):
+    """ Service to implement the Reverification XBlock "reverification" service
+
+    """
 
     def get_status(self, user_id, course_id, checkpoint_name):
+        """ Check if the user has any verification attempt for this checkpoint and course_id
+
+        Args:
+            user_id(str): User Id string
+            course_id(str): A string of course_id
+            checkpoint_name(str): Verification checkpoint name
+
+        Returns:
+            Verification Status string if any attempt submitted by user else None
+        """
         course_key = CourseKey.from_string(course_id)
         try:
             checkpoint_status = VerificationStatus.objects.filter(
@@ -19,6 +32,15 @@ class ReverificationService(object):
             return None
 
     def start_verification(self, course_id, checkpoint_name, item_id):
+        """ Get or create the verification checkpoint and return the re-verification link
+
+        Args:
+            course_id(str): A string of course_id
+            checkpoint_name(str): Verification checkpoint name
+
+        Returns:
+            Re-verification link
+        """
         course_key = CourseKey.from_string(course_id)
         VerificationCheckpoint.objects.get_or_create(course_id=course_key, checkpoint_name=checkpoint_name)
         re_verification_link = reverse("verify_student_incourse_reverify", args=(course_id, checkpoint_name))
